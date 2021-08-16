ed97d49d987b5d64976555134a0d6769ab794fc7
Sajid Akhtar <sajid.i.akhtar@gmail.com>
2021-08-16 22:21:14 +0400

    dashboard my acount view, my account link in navbar, modify foother text

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,6 @@
 class DashboardController < ApplicationController
+  skip_after_action :verify_authorized, only: [:my_account]
+
   def my_account
     # current_user.bookings
     # current_user.spaces
