ea909b1229532e415bd283c4756b8b1c27f59d90
Sajid Akhtar <sajid.i.akhtar@gmail.com>
2021-08-18 15:42:43 +0400

    fix JS with guard clause n map and flatpickr, booking form price

--- app/javascript/components/init_flatpickr.js
+++ app/javascript/components/init_flatpickr.js
@@ -3,15 +3,19 @@ import flatpickr from "flatpickr";
 const initFlatpickr = () => {
 
   const startDateInput = document.getElementById('booking_date_from');
+  const totalPrice = document.getElementById('total_price');
+  let spacePrice
+  let bookingCalendar
 
   if (startDateInput) {
+    spacePrice = parseFloat(totalPrice.dataset.price)
     const bookingForm = document.getElementById('booking-dates')
     const hasData = bookingForm.hasAttribute('data-unavailable');
 
     if (hasData) {
       const unavailableDates = JSON.parse(bookingForm.dataset.unavailable)
 
-      flatpickr(startDateInput, {
+      bookingCalendar = flatpickr(startDateInput, {
         minDate: "today",
         disable: unavailableDates,
         altInput: true,
@@ -21,7 +25,7 @@ const initFlatpickr = () => {
       });
 
     } else {
-      flatpickr(startDateInput, {
+      bookingCalendar = flatpickr(startDateInput, {
         minDate: "today",
         altInput: true,
         altFormat: "F j, Y",
@@ -31,7 +35,19 @@ const initFlatpickr = () => {
 
     }
 
+    bookingCalendar.config.onChange.push((selectedDates) => {
+      if (selectedDates.length === 2) {
+        const dateFrom = new Date(selectedDates[0]);
+        const dateTo = new Date(selectedDates[1]);
+        const diffTime = Math.abs(dateTo - dateFrom);
+        const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24)) + 1;
+        const total = spacePrice * diffDays
+        totalPrice.innerText = `Total: AED ${total}`
+      }
+    });
+
   };
+
 }
 
 export { initFlatpickr }
