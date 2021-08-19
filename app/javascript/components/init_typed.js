ff3e1af4480b86b40445c76958fc735ae10336d1
Sajid Akhtar <sajid.i.akhtar@gmail.com>
2021-08-19 07:58:49 +0400

    add typed js closed guard

--- app/javascript/components/init_typed.js
+++ app/javascript/components/init_typed.js
@@ -1,15 +1,19 @@
 import Typed from 'typed.js';
 
 const initTyped = () => {
-  const options = {
-    strings: ['Offices', 'Conference Rooms','Event Halls'],
-    typeSpeed: 150,
-    startDelay: 500,
-    loop: true
-  };
 
-  const typed = new Typed('.typed', options);
+  const typedElement = document.querySelector('.typed');
 
+  if(typedElement) {
+    const options = {
+      strings: ['Offices', 'Conference Rooms','Event Halls'],
+      typeSpeed: 150,
+      startDelay: 500,
+      loop: true
+    };
+
+    const typed = new Typed('.typed', options);
+  }
 }
 
 export { initTyped }
