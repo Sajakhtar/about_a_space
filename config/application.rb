421c1a527ff0b5d7498cad618b3cd3c3817e7c6b
Lanre Fabunmi <lanref@gmail.com>
2021-08-25 11:48:05 +0400

    resolving issues

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,7 @@ Bundler.require(*Rails.groups)
 module AboutASpace
   class Application < Rails::Application
     config.action_view.embed_authenticity_token_in_remote_forms = true
+    config.assets.initialize_on_precompile = false
     config.generators do |generate|
       generate.assets false
       generate.helper false
@@ -22,7 +23,6 @@ module AboutASpace
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    config.assets.initialize_on_precompile = false
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
   end
